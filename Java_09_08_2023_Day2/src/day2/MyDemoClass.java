e87adf4e9bf62e0f12eeadc76e341a7a3e0f833b
Kavitha-nd <kavitha@navadhiti.com>
2023-08-09 12:01:42 +0530

    Datatypes

---
+++ Java_09_08_2023_Day2/src/day2/MyDemoClass.java
@@ -0,0 +1,29 @@
+package day2;
+
+public class MyDemoClass {
+
+	public static void main(String[] args) {
+		
+		
+		//Data_types & Variables
+		
+		int myVar = 10;
+		System.out.println(myVar);
+		
+		int age = 20;
+		System.out.println(age);
+		
+		float money = 1000.5f;
+		System.out.println(money);
+		
+		double totalMoney = 200.5d;
+		System.out.println(totalMoney);
+		
+		String TotalMoney ="$3000 dollar";
+		System.out.println(TotalMoney);
+		
+		char dollar ='$';  //char-single character ,store memory less than String
+		System.out.println(dollar);
+	}
+
+}
